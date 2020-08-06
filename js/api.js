dae7be886149bf06ea925b04dbd219985402aafa
Daniel <danyelcamusso@gmail.com>
2020-08-06 19:49:04 -0300

    comenzando js

---
+++ js/api.js
@@ -0,0 +1,17 @@
+export class API{
+
+    constructor(apikey){
+        this.apikey = apikey;
+    }
+
+    async tendenciaGifs(offset = 0){
+
+        const url = `https://api.giphy.com/v1/gifs/trending?api_key=${this.apikey}&limit=24&rating=G&offset=${offset}`;
+
+        const pedirTendencias = await fetch(url);
+
+        const tendencias = await pedirTendencias.json();
+
+        return tendencias;
+    }
+}
---
+++ js/app.js
@@ -0,0 +1,3 @@
+import{API} from "./api.js";
+
+export const apiGifs = new API ('9irbiK8l658g0xVfQOoCAh6M94vBGIGr');
