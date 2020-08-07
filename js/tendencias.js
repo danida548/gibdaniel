e8a87a098727dde72bb69f0fd8c8588ea58fd994
Daniel <danyelcamusso@gmail.com>
2020-08-07 00:13:26 -0300

    tarjetas-seccion de gif

---
+++ js/tendencias.js
@@ -0,0 +1,42 @@
+import {apiGifs} from "./app.js";
+
+export class TENDENCIAS{
+    constructor(){
+        this.tendencias = document.querySelector('.tendencias');
+        this.init();
+    }
+    init(){
+        this.mostrarTendencias()
+    }
+    mostrarTendencias(){
+
+        apiGifs.tendenciaGifs()
+            .then(tendencia =>{
+                const tendencias = tendencia.data;
+                this.crearTendencia(tendencias);
+            });
+    }
+
+    crearTendencia(tendencias){
+
+        for (let value of tendencias) {
+           
+            const tituloImagen = value.title.split(" ")[0],
+                  tarjeta = document.createElement("div"),
+                  div2 = document.createElement("div"),
+                  gif = document.createElement("img");
+                  
+            gif.src = value.images.original.url;
+            tarjeta.className = "tarjeta";
+            gif.className = "gif";
+
+            if(value.images.original.url.width > 480){
+                tarjeta.classList.add("grande")
+            }
+            
+            this.tendencias.lastElementChild.appendChild(tarjeta)
+            tarjeta.appendChild(div2, gif);
+            tarjeta.appendChild(gif);
+        }
+    }
+}
